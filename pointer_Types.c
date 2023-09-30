d212827e0057c1d9efe86d95c65908925b27c1f4
Waseem <waseemirfan2019@gmail.com>
2023-09-30 21:35:21 +0500

    Added Arithmatic & Generic pointer

--- pointer_Types.c
+++ pointer_Types.c
@@ -5,8 +5,11 @@ int main(void){
     int *p = &a;
     printf("Size of integer = %d\n",sizeof(int));
     printf("Address = %d and Value = %d\n",p,*p);
-    char *p0 =(char*) p;
-    printf("Size of character = %d\n",sizeof(int));
+    printf("Address = %d and Value = %d\n",p+1,*(p+1));
+    char *p0 =(char*) p; // Type Casting
+    printf("Size of character = %d\n",sizeof(char));
     printf("Address = %d and Value = %d\n",p0,*p0);
+    printf("Address = %d and Value = %d\n",p0+1,*(p0+1));
+    // void *p; // Generic pointer
     return 0;
 }
