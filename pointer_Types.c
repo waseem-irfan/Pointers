85a697f2a7017abb7ccd4ef07c47750e957906a8
Waseem <waseemirfan2019@gmail.com>
2023-09-30 21:23:16 +0500

    pointer types commit

---
+++ pointer_Types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 1033;
+    int *p = &a;
+    printf("Size of integer = %d\n",sizeof(int));
+    printf("Address = %d and Value = %d\n",p,*p);
+    char *p0 = p;
+    printf("Size of character = %d\n",sizeof(int));
+    printf("Address = %d and Value = %d\n",p0,*p0);
+    return 0;
+}
